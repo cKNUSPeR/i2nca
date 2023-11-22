634316821091bd4cd199c9f6e5cb82bcf3b4d3bf
cKNUSPeR <108276092+cKNUSPeR@users.noreply.github.com>
2023-11-22 18:44:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name='i2nca',
     version='0.1.0',
     packages=find_packages(include=['i2nca', 'i2nca.*', 'i2nca.qctools.*']),
-    py_modules=['qctools.qctools','qctools.visualization', 'qctools.utils', 'qctools.dependencies'],
     install_requires=[
         'm2aia==0.5.1',
     ]    
