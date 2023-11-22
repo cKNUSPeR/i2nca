42cf133d054ae4defe81289a8fb39297ee77ac83
cKNUSPeR <108276092+cKNUSPeR@users.noreply.github.com>
2023-11-22 15:46:39 +0100

    Update __init__.py

--- i2nca/__init__.py
+++ i2nca/__init__.py
@@ -1,6 +1,6 @@
 """inca - INteractive quality Control and Assesment using m2aia"""
 
-from i2nca.qctools import report_agnostic_qc, report_calibrant_qc, report_regions_qc
+from qctools.qctools import report_agnostic_qc, report_calibrant_qc, report_regions_qc
 
 __author__ = "Jannik Witte"
 
