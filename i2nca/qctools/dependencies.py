54968d5dbb05a7a246b5db49ca497a08252653b9
cKNUSPeR <108276092+cKNUSPeR@users.noreply.github.com>
2023-11-22 15:47:24 +0100

    Create dependencies.py
    upload dependencies

---
+++ i2nca/qctools/dependencies.py
@@ -0,0 +1,15 @@
+# namespace and import declaration
+import matplotlib.backends as mpb
+import m2aia as m2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import random as rnd
+import statistics as stat
+import scipy.stats as SST
+import scipy.signal as SSI
+import skimage.measure as skim
+import pandas as pd
+import warnings
+from scipy.signal import argrelextrema
+import matplotlib.backends.backend_pdf
