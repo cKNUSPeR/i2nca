ae433a60ade51129e09f505801dbbfd80c8fab99
cKNUSPeR <108276092+cKNUSPeR@users.noreply.github.com>
2023-11-22 15:45:25 +0100

    Create __init__.py
    Upload __init

---
+++ i2nca/__init__.py
@@ -0,0 +1,7 @@
+"""inca - INteractive quality Control and Assesment using m2aia"""
+
+from i2nca.qctools import report_agnostic_qc, report_calibrant_qc, report_regions_qc
+
+__author__ = "Jannik Witte"
+
+__version__ = "2.0.0"
