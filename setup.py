e6e9551bcf6f260dd948fe6114fee26211333774
cKNUSPeR <108276092+cKNUSPeR@users.noreply.github.com>
2023-12-13 17:47:46 +0100

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='i2nca',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(include=['i2nca','i2nca.qctools']),
     install_requires=[
         'm2aia==0.5.1',
