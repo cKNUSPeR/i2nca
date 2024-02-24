fd9637bd809d5a31384c75a4364e5473ae73b7fe
cKNUSPeR <108276092+cKNUSPeR@users.noreply.github.com>
2024-02-24 18:56:18 +0100

    Update __init__.py

--- i2nca/brukertools/__init__.py
+++ i2nca/brukertools/__init__.py
@@ -1,1 +1,7 @@
-"""inca - ms Imaging INteractive quality Control and Assesment using m2aia tools distro"""
+"""i2nca"""
+
+# tool set for pp to pp imzml
+from i2nca.brukertools.tdf_tools import TdfReader, convert_tdf_to_pc_imzml
+
+# imports for pp to cp imzml
+from i2nca.brukertools.tsf_tools import TsfReader, convert_tsf_to_pp_imzml
