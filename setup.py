1af6d004aeff86dc5e06414b6fd305093d340b67
Jannik <jw1217@gmx.de>
2024-05-02 16:49:29 +0200

    updated to version 0.2.5

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='i2nca',
-    version='0.2.4',
-    packages=find_packages(include=['i2nca', 'i2nca.qctools', 'i2nca.convtools', 'i2nca.brukertools']),
+    version='0.2.5',
+    packages=find_packages(include=['i2nca', 'i2nca.qctools', 'i2nca.convtools']),
     install_requires=[
         'numpy<1.25',
         'matplotlib<3.8',
