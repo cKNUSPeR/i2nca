6a04d3d7f375184c97eae7a514016fcfd1542117
Jannik <jw1217@gmx.de>
2024-05-02 16:49:29 +0200

    updated to version 0.2.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='i2nca',
-    version='0.2.4',
+    version='0.2.5',
     packages=find_packages(include=['i2nca', 'i2nca.qctools', 'i2nca.convtools']),
     install_requires=[
         'numpy<1.25',
