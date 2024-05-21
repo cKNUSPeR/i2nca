86b134e8ef973676f60332158639a8bfaa10a3a6
Jannik <jw1217@gmx.de>
2024-05-21 13:16:03 +0200

    brukertoolsin setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='i2nca',
     version='0.2.5',
-    packages=find_packages(include=['i2nca', 'i2nca.qctools', 'i2nca.convtools']),
+    packages=find_packages(include=['i2nca', 'i2nca.qctools', 'i2nca.convtools', 'i2nca.brukertools']),
     install_requires=[
         'numpy<1.25',
         'matplotlib<3.8',
