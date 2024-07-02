56e13b5156b7e23464956693118e90c786f83eab
Jannik <jw1217@gmx.de>
2024-07-02 17:02:23 +0200

    Added a get_version function to test builds in enviroment

--- i2nca/__init__.py
+++ i2nca/__init__.py
@@ -6,12 +6,13 @@ __version__ = "0.2.9"
 
 # registery of main function for i2nca namespace, each defined over their own group.
 
+from i2nca.main import get_version
+
 from i2nca.qctools import *
 
 from i2nca.convtools import *
 
 # from i2nca.brukertools import *
-
 
 # bruker tools are excluded here. checkout the branch "bruker inclusive" to get acces to these functions.
 
--- i2nca/main.py
+++ i2nca/main.py
@@ -1,25 +1,8 @@
+"""inca - INteractive quality Control and Assesment using m2aia"""
 
-import m2aia as m2
-import matplotlib.pyplot as plt
-from qctools.qc_tools import report_agnostic_qc, report_calibrant_qc, report_regions_qc
-import webbrowser
+__author__ = "Jannik Witte"
 
+__version__ = "0.2.9"
 
-if __name__ == "__main__":
-
-    file_name = r"C:\Users\Jannik\Documents\Uni\Master_Biochem\4_Semester\QCdev\src\i2nca\i2nca\tests\testdata\cc.imzML"
-
-    I = m2.ImzMLReader(file_name)
-
-    fig = plt.figure(figsize=[7, 5])
-    ax = plt.subplot(111)
-    ax.set_title(
-        f'Intensity of 78.959, "mz"] ± {50} ppm')
-    ax.set_xlabel('x axis')
-    ax.set_ylabel('y axis')
-
-    im = ax.imshow(I.GetArray(78.959, tol=50)[0],
-                   cmap="viridis")
-    fig.show()
-
-    print(I.GetArray(78.959, tol=50)[0])
+def get_version():
+    return __version__
