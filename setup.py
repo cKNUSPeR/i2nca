8ec7b9b8f0e454bcb87dd40db868c95c081ffc7b
Jannik <jw1217@gmx.de>
2024-07-10 13:51:58 +0200

    Prelim commit to test CLI, with two cli

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='i2nca',
     version='0.2.9',
-    packages=find_packages(include=['i2nca', 'i2nca.qctools', 'i2nca.convtools']),
+    packages=find_packages(include=['i2nca', 'i2nca.qctools', 'i2nca.convtools', 'i2nca.workflows', 'i2nca.tests']),
     entry_points={
         'console_scripts': [
             'i2nca_version = i2nca.main:get_version',
+            'i2nca_agnostic_qc = i2nca.workflows.CLI.agnostic_qc_cli',
         ]
     },
     install_requires=[
