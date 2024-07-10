8c72b6ac84d05ce5f50eaf32c127166d362eb9c7
Jannik <jw1217@gmx.de>
2024-07-10 11:37:09 +0200

    Prelim commit to test CLI

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ setup(
     name='i2nca',
     version='0.2.9',
     packages=find_packages(include=['i2nca', 'i2nca.qctools', 'i2nca.convtools']),
+    entry_points={
+        'console_scripts': [
+            'i2nca_version = i2nca.main:get_version',
+        ]
+    },
     install_requires=[
         'numpy<1.25',
         'matplotlib<3.8',
